fe4e3d048e4d76dfe96cf6c1b0b765be4cd0caaf
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-31 22:32:23 +0700

    Menambah fungsi tambah tugas dan check tugas

--- index.js
+++ index.js
@@ -8,25 +8,49 @@ const input = readlinePromises.createInterface({
 })
 
 async function main(){
-    console.log("Selamat datang di aplikasi Tugas-Turu");
-    console.log("========================================")
-    let username = await input.question("Silahkan masukkan usernamemu : ");
-    let password = await input.question("Masukkan password akunmu : ");
-    let hasilLogin = await login(username,password);
-    if(hasilLogin===false){
-        console.log("Kamu masih belum memiliki akun");
-        let jawaban = await input.question("Apakah kamu ingin membuat akun baru (ya/tidak)? ");
-        if(jawaban.toLowerCase()=="ya"){
-            let usernameBaru = await input.question("Masukkan username yang ingin kamu gunakan : ");
-            let passwordBaru = await input.question("Masukkan password yang ingin kamu gunakan : ");
-            let status = await addUser(usernameBaru,passwordBaru);
-            console.log(status);
+    let looping = true;
+    while(looping){
+        console.log("Selamat datang di aplikasi Tugas-Turu");
+        console.log("========================================")
+        let username = await input.question("Silahkan masukkan usernamemu : ");
+        let password = await input.question("Masukkan password akunmu : ");
+        let hasilLogin = await login(username,password);
+        if(hasilLogin===false){
+            console.log("Kamu masih belum memiliki akun");
+            let jawaban = await input.question("Apakah kamu ingin membuat akun baru (ya/tidak)? ");
+            if(jawaban.toLowerCase()=="ya"){
+                let usernameBaru = await input.question("Masukkan username yang ingin kamu gunakan : ");
+                let passwordBaru = await input.question("Masukkan password yang ingin kamu gunakan : ");
+                let status = await addUser(usernameBaru,passwordBaru);
+                console.log(status);
+            }else{
+                console.log("Keluar dari program");
+                process.exit(1);
+            }
         }else{
-            console.log("Keluar dari program");
-            process.exit(1);
+            console.log(`Selamat datang ${username}, silahkan menggunakan aplikasi ini`);
+            while(true){
+                const opsi = ["Tambah Tugas", "Hapus Tugas","List Tugas","Keluar Dari Program"];
+                let statusCheck = await check(username,password);
+                if(statusCheck==false){
+                    console.log("Anda bisa turu sekarang karena tidak ada tugas :)");
+                }
+                for (let i in opsi){
+                    console.log(`${Number(i)+1} ${opsi[i]}`);
+                }
+                let perintah = await input.question("Masukkan perintah yang kamu inginkan (dalam bentuk angka) : ");
+                if(Number(perintah)==1){
+                    let namaTugas = await input.question("Masukkan nama tugas : ");
+                    let matkul = await input.question("Masukkan mata kuliah dari tugas : ");
+                    let statusTambah = await addTask(username,password,namaTugas,matkul);
+                    console.log(statusTambah);
+                }else if(Number(perintah)==4){
+                    console.log("Keluar dari layanan");
+                    break;
+                }
+            }
+
         }
-    }else{
-        console.log(`Selamat datang ${username}, silahkan menggunakan aplikasi ini`);
     }
 }
 
@@ -65,6 +89,28 @@ async function addUser(username,password){
         "poin":0
     })
     return "Selamat, akun anda telah ditambahkan";
+}
+
+
+async function addTask(username,password,namaTugas,matkul){
+    const dataMahasiswa = await fetchData();
+    const tugasBaru = {
+        "namaTugas":namaTugas,
+        "matkul":matkul
+    }
+    await dataMahasiswa.updateOne({nama:username,password:password},{$push:{tugas:tugasBaru}});
+    return "tugas berhasil ditambah";
+}
+
+async function check(username,password){
+    const dataMahasiswa = await fetchData();
+    const tampungan = await dataMahasiswa.findOne({nama:username,password:password},{tugas:1});
+    const arrayTugas = tampungan.tugas;
+    if(arrayTugas.length==0){
+        return false;
+    }else{
+        return true;
+    }
 }
 
 main();
