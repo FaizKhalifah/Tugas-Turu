2fd886467a3086e51abbd0ac4a4761f9ae61610a
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-31 13:40:59 +0700

    testing connect ke database

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ import express from "express";
 import { promises as fsPromises } from 'fs';
 import process from "process";
 import readlinePromises from "readline/promises";
+import { MongoClient } from 'mongodb';
 
 const input = readlinePromises.createInterface({
     input:process.stdin,
     output:process.stdout
 })
 
+const client = new MongoClient('mongodb://localhost:27017/Latihan1');
+
+let mahasiswa;
+let kumpulanMahasiswa;
+
+async function fetchData(){
+    await client.connect();
+    console.log('Terhubung ke database');
+
+    const db = client.db();
+    mahasiswa = db.collection('mahasiswa');
+    kumpulanMahasiswa = await mahasiswa.find().toArray();
+    return kumpulanMahasiswa;
+}
+
 const app = express();
+
 app.listen(3000,()=>{
     console.log("Server menyala");
-});
-
-app.get('/mahasiswa',(req,res)=>{
-    res.json({
-        pesan:"Selamat datang di API"
-    })
 })
 
+let dataMahasiswa = await fetchData();
+console.log(dataMahasiswa);
